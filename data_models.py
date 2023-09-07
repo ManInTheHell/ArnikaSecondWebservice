0f8bbf2848dc78effcc37f89322585360bf3bb1b
ManInTheHell <mohammad.hosein1411@gmail.com>
2023-09-07 16:41:24 +0330

    add max and min processing and inserting to db part

--- data_models.py
+++ data_models.py
@@ -23,3 +23,35 @@ class StandardDeviation(db.Model):
     std_deviation_b = db.Column(db.Float, nullable=True)
     value_id = db.Column(db.Integer, db.ForeignKey('values.id'))
     value = db.relationship('Values', backref='std_deviation')
+
+
+class MaxDiffA(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    max_diff_a = db.Column(db.Float, nullable=True)
+    datetime = db.Column(db.DateTime, nullable=False)
+    value_id = db.Column(db.Integer, db.ForeignKey('values.id'))
+    value = db.relationship('Values', backref='max_diff_a')
+
+
+class MinDiffA(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    min_diff_a = db.Column(db.Float, nullable=True)
+    datetime = db.Column(db.DateTime, nullable=False)
+    value_id = db.Column(db.Integer, db.ForeignKey('values.id'))
+    value = db.relationship('Values', backref='min_diff_a')
+
+
+class MaxDiffB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    max_diff_b = db.Column(db.Float, nullable=True)
+    datetime = db.Column(db.DateTime, nullable=False)
+    value_id = db.Column(db.Integer, db.ForeignKey('values.id'))
+    value = db.relationship('Values', backref='max_diff_b')
+
+
+class MinDiffB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    min_diff_b = db.Column(db.Float, nullable=True)
+    datetime = db.Column(db.DateTime, nullable=False)
+    value_id = db.Column(db.Integer, db.ForeignKey('values.id'))
+    value = db.relationship('Values', backref='min_diff_b')
--- task_one_methods.py
+++ task_one_methods.py
@@ -8,6 +8,7 @@ from app import app, db
 def task_one():
     a, b, fetch_time = call_data_web_service()
     new_values_id = insert_data_to_db(a, b, fetch_time)
+    # new_values_id = 1000
     statistics_processes(new_values_id)
 
 
@@ -29,15 +30,16 @@ def insert_data_to_db(a, b, fetch_time):
 
 def statistics_processes(new_values_id):
     raw_data_df = fetch_raw_data_from_db()
-    correlation(raw_data_df, new_values_id)
-    standard_deviation(raw_data_df, new_values_id)
+    # correlation(raw_data_df, new_values_id)
+    # standard_deviation(raw_data_df, new_values_id)
+    max_min_diff(raw_data_df)
 
 
 def fetch_raw_data_from_db():
     with app.app_context():
-        query = db.session.query(Values.value_a, Values.value_b)
+        query = db.session.query(Values.id, Values.value_a, Values.value_b, Values.datetime)
         results = query.all()
-        df = pd.DataFrame(results, columns=['Value_a', 'Value_b'])
+        df = pd.DataFrame(results, columns=['id', 'Value_a', 'Value_b', 'datetime'])
         return df
 
 
@@ -47,7 +49,9 @@ def correlation(df, new_values_id):
 
 
 def calculate_correlation(df):
-    correlation_matrix = df.corr()
+    just_values_df = df.copy()
+    just_values_df.drop(columns=['id', 'datetime'], inplace=True)
+    correlation_matrix = just_values_df.corr()
     return correlation_matrix.loc['Value_a', 'Value_b']
 
 
@@ -67,13 +71,16 @@ def standard_deviation(df, new_values_id):
 
 
 def calculate_standard_deviation(df):
-    standard_deviation_values = df.std(axis=0)
+    just_values_df = df.copy()
+    just_values_df.drop(columns=['id', 'datetime'], inplace=True)
+    standard_deviation_values = just_values_df.std(axis=0)
     standard_deviation_a = standard_deviation_values['Value_a']
     standard_deviation_b = standard_deviation_values['Value_b']
     return standard_deviation_a, standard_deviation_b
 
 
 def fill_standard_deviation_table(value_a, value_b, new_values_id):
+    # todo: use tuple
     with app.app_context():
         if str(value_a) != 'nan':
             new_standard_deviation = StandardDeviation(std_deviation_a=value_a, std_deviation_b=value_b,
@@ -83,3 +90,79 @@ def fill_standard_deviation_table(value_a, value_b, new_values_id):
                                                        value_id=new_values_id)
         db.session.add(new_standard_deviation)
         db.session.commit()
+
+
+def max_min_diff(df):
+    if len(df) > 1:
+        max_min_diff_tuple_values = calculate_max_min_diff(df)
+        fill_max_min_diff_tables(max_min_diff_tuple_values)
+
+
+def calculate_max_min_diff(df):
+    copy_df = df.copy()
+    copy_df = copy_df.sort_values(by='datetime')
+    copy_df['delta_a'] = copy_df['Value_a'].diff()
+    copy_df['delta_b'] = copy_df['Value_b'].diff()
+
+    max_diff_a = copy_df.loc[copy_df['delta_a'].idxmax()]
+    min_diff_a = copy_df.loc[copy_df['delta_a'].idxmin()]
+    max_diff_b = copy_df.loc[copy_df['delta_b'].idxmax()]
+    min_diff_b = copy_df.loc[copy_df['delta_b'].idxmin()]
+    return max_diff_a, min_diff_a, max_diff_b, min_diff_b
+
+
+def fill_max_min_diff_tables(args):
+    last_records = fetch_last_records_value_id()
+    if last_records[0] != args[0]['id']:
+        with app.app_context():
+            new_max_diff_a = MaxDiffA(max_diff_a=args[0]['delta_a'], datetime=args[0]['datetime'],
+                                      value_id=int(args[0]['id']))
+            db.session.add(new_max_diff_a)
+            db.session.commit()
+
+    if last_records[1] != args[1]['id']:
+        with app.app_context():
+            new_min_diff_a = MinDiffA(min_diff_a=args[1]['delta_a'], datetime=args[1]['datetime'],
+                                      value_id=int(args[1]['id']))
+            db.session.add(new_min_diff_a)
+            db.session.commit()
+
+    if last_records[2] != args[2]['id']:
+        with app.app_context():
+            new_max_diff_b = MaxDiffB(max_diff_b=args[2]['delta_b'], datetime=args[2]['datetime'],
+                                      value_id=int(args[2]['id']))
+            db.session.add(new_max_diff_b)
+            db.session.commit()
+
+    if last_records[3] != args[3]['id']:
+        with app.app_context():
+            new_min_diff_b = MinDiffB(min_diff_b=args[3]['delta_b'], datetime=args[3]['datetime'],
+                                      value_id=int(args[3]['id']))
+            db.session.add(new_min_diff_b)
+            db.session.commit()
+
+
+def fetch_last_records_value_id():
+    with app.app_context():
+        latest_record_max_diff_a = db.session.query(MaxDiffA).order_by(MaxDiffA.datetime.desc()).first()
+        latest_record_min_diff_a = db.session.query(MinDiffA).order_by(MinDiffA.datetime.desc()).first()
+        latest_record_max_diff_b = db.session.query(MaxDiffB).order_by(MaxDiffB.datetime.desc()).first()
+        latest_record_min_diff_b = db.session.query(MinDiffB).order_by(MinDiffB.datetime.desc()).first()
+        if latest_record_max_diff_a is None:
+            latest_record_max_diff_a = 0
+        else:
+            latest_record_max_diff_a = latest_record_max_diff_a.value_id
+        if latest_record_min_diff_a is None:
+            latest_record_min_diff_a = 0
+        else:
+            latest_record_min_diff_a = latest_record_min_diff_a.value_id
+        if latest_record_max_diff_b is None:
+            latest_record_max_diff_b = 0
+        else:
+            latest_record_max_diff_b = latest_record_max_diff_b.value_id
+        if latest_record_min_diff_b is None:
+            latest_record_min_diff_b = 0
+        else:
+            latest_record_min_diff_b = latest_record_min_diff_b.value_id
+
+    return latest_record_max_diff_a, latest_record_min_diff_a, latest_record_max_diff_b, latest_record_min_diff_b
